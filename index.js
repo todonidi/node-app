bf176d89ae7c8f07007833a886c064b2bb91e7d4
root <root@webserver04.storage.telkomakses.co.id>
2019-09-30 09:43:56 +0700

    edit index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world testing edit');
+ res.send('hello world node-app');
 });//listen to port 5000 by default
 app.listen(process.env.PORT || 5000);
  
